105d38382ad1735296d456c83dfffb409fc1cc1b
Carolyn McNeillie <cmcneillie@theworkinggroup.ca>
2019-12-10 21:13:04 -0500

    basic flip set up on card component

--- src/common/components/Card/Card.js
+++ src/common/components/Card/Card.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 
 import styles from "./Card.module.scss";
 
 export const Card = ({ name, face, children }) => {
+  const [isFaceUp, setFaceUp] = useState(false);
+
+  const handleFlip = e => {
+    if (!isFaceUp) {
+      setFaceUp(true);
+      console.log(e.target.getAttribute("data-name"));
+      setTimeout(function() {
+        setFaceUp(false);
+      }, 1500);
+    }
+  };
+
+  const cardClassName = classNames(styles.card, {
+    [styles["card--faceUp"]]: isFaceUp
+  });
+
   return (
-    <article className={styles.card} data-name={name}>
-      {children}
+    <article className={cardClassName} data-name={name} onClick={handleFlip}>
+      <span className={styles.card__face}>{children}</span>
     </article>
   );
 };
 
 Card.propTypes = {
   name: PropTypes.string.isRequired,
-  face: PropTypes.string.isRequired,
   faceUp: PropTypes.bool
 };
 

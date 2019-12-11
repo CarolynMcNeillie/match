cba60168e2efc458ff46b01e60e96c708a40f95f
Carolyn McNeillie <cmcneillie@theworkinggroup.ca>
2019-12-11 11:29:51 -0500

    Moved shuffleArray and doubleArray to utils

--- src/common/index.js
+++ src/common/index.js
@@ -1,3 +1,7 @@
 export * from './components/Accordion'
 export * from './components/Button.js'
 export * from './components/Card'
+export * from './components/cardFaces.js'
+
+export * from './utils/doubleArray.js'
+export * from './utils/shuffleArray.js'
--- src/core/Board.js
+++ src/core/Board.js
@@ -1,34 +1,11 @@
 import React from "react";
 
-import { Card } from "common/components";
-import { CardFaces } from "common/components/CardFaces";
+import { Card, cardFaces, shuffleArray, doubleArray } from "common";
 
 import styles from "./Board.module.scss";
 
 export const Board = () => {
-  const doubleCards = arr => {
-    return arr.concat(arr);
-  };
-
-  //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-  const shuffle = arr => {
-    let currentIndex = arr.length;
-    let temporaryValue = 0;
-    let randomIndex = 0;
-
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      temporaryValue = arr[currentIndex];
-      arr[currentIndex] = arr[randomIndex];
-      arr[randomIndex] = temporaryValue;
-    }
-
-    return arr;
-  };
-
-  const randCards = shuffle(doubleCards(CardFaces));
+  const randCards = shuffleArray(doubleArray(cardFaces));
 
   return (
     <div className={styles.board}>

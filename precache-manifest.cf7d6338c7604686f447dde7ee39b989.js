12e953705b272e96a55e55b1056c506bd617493d
Carolyn McNeillie <cmcneillie@theworkinggroup.ca>
2019-12-23 22:17:58 -0500

    Updates

---
+++ precache-manifest.cf7d6338c7604686f447dde7ee39b989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a814fdd8aec435d6c0e6050b06173b8",
+    "url": "/match/index.html"
+  },
+  {
+    "revision": "4054e1669c18703d65d7",
+    "url": "/match/static/css/main.95565e84.chunk.css"
+  },
+  {
+    "revision": "0dedd1224c0c6bedaaa0",
+    "url": "/match/static/js/2.62e25d2c.chunk.js"
+  },
+  {
+    "revision": "4054e1669c18703d65d7",
+    "url": "/match/static/js/main.3e7b1980.chunk.js"
+  },
+  {
+    "revision": "29afe1a9853b549f4446",
+    "url": "/match/static/js/runtime-main.869cb5a7.js"
+  }
+]);
